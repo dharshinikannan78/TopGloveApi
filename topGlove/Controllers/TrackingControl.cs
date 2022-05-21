41713238ea7bbaaa5abd88e81cdb2c204c4da190
salmank07 <mohamedsalmankhan509@gmail.com>
2022-05-21 17:15:49 +0530

    initial

---
+++ topGlove/Controllers/TrackingControl.cs
@@ -0,0 +1,117 @@
+﻿using ClosedXML.Excel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using topGlove.Data;
+using topGlove.Extension;
+using topGlove.Model;
+
+
+namespace topGlove.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TrackingControl : ControllerBase
+    {
+        public readonly UserDbContext dataContext;
+
+        public TrackingControl(UserDbContext userData)
+        {
+            dataContext = userData;
+        }
+
+        [HttpPost("AddTrayDetail")]
+
+        public IActionResult AddTrayDetail([FromBody] TrayTrackinInput inputOperatorData)
+        {
+            if (inputOperatorData == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                dataContext.TrayDetails.Add(inputOperatorData);
+                dataContext.SaveChanges();
+                return Ok(inputOperatorData);
+              
+            }           
+        }
+
+        [HttpPut("UpdateTrayDetail")]
+
+        public IActionResult UpdateTrayDetail([FromBody] TrayTrackinInput inputData)
+        {
+            if(inputData == null)
+            {
+                return NotFound();
+            }
+            var details = dataContext.TrayDetails.AsNoTracking().FirstOrDefault(x=>x.UserId == inputData.UserId);
+            if (details == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                dataContext.Entry(inputData).State = EntityState.Modified;
+                dataContext.SaveChanges();
+                return Ok(inputData);
+            }
+        }
+
+        [HttpDelete("DeleteTrayDetail")]
+
+        public IActionResult DeleteTrayDetail(int id)
+        {
+            var inputData = dataContext.TrayDetails.Find(id);
+            if(inputData == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                dataContext.TrayDetails.Remove(inputData);
+                dataContext.SaveChanges();
+                return Ok();
+            }
+        }
+
+        [HttpPost("FilteredItems")]
+        public IActionResult GetProductQualityWithUser(RequestModel requestModel)
+        {
+            var response = GetFilter(requestModel);
+            return Ok(response);
+        }
+
+        [HttpPost("GenerateExcel")]
+        public IActionResult GenerateExcel(RequestModel requestModel)
+        {
+            var response = GetFilter(requestModel);
+            response = response.OrderByDescending(x => x.UserId).ToList();
+            var excelStream = response.CreateExcel<TrayTrackinInput>();
+
+            var fileName = $"TopGlove_{DateTime.Now}.xlsx";
+            return File(excelStream,
+                        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                        fileName);
+        }
+
+        private List<TrayTrackinInput> GetFilter(RequestModel requestModel)
+        {
+            var response = dataContext.TrayDetails.Where(a => a.DataTime.Date >= requestModel.FromDate.Date && a.DataTime.Date
+            <= requestModel.ToDate.Date);
+            if(!string.IsNullOrWhiteSpace(requestModel.User) && response.Any())
+            {
+                response = response.Where(a => a.User == requestModel.User);
+            }
+            return response.ToList();
+        }
+        
+    }
+}
+
---
+++ topGlove/Controllers/UserController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using topGlove.Data;
+using topGlove.Model;
+
+namespace topGlove.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public readonly UserDbContext dataContext;
+
+        public UserController(UserDbContext userData)
+        {
+            dataContext = userData;
+        }
+
+
+        [HttpPost("AddUser")]
+
+        public IActionResult AddUser( [FromBody] UserDetails userData )
+        {
+           
+            if(userData == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                dataContext.Login.Add(userData);
+                dataContext.SaveChanges();
+                return Ok(userData);
+            }
+        }
+
+
+      [HttpPost("GetLogin")]
+      public IActionResult GetLogin([FromBody] UserDetails data)
+        {
+            var user = dataContext.Login.Where(x => x.UserName == data.UserName && x.Password == data.Password).FirstOrDefault();
+            if (user.Role == "Admin")
+            {            
+                var userList = dataContext.Login.AsQueryable();
+                return Ok(userList);
+            }
+            if(user.Role == "operator")
+            {
+                return Ok(user);
+            }
+            return BadRequest();
+        }
+
+        [HttpPut("UpdateLogin")]
+
+        public IActionResult UpdateLogin([FromBody] UserDetails obj)
+        {
+            if (obj == null)
+            {
+                return BadRequest();
+            }
+            var user = dataContext.Login.AsNoTracking().FirstOrDefault(x=>x.UserId == obj.UserId);
+            if (user == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                dataContext.Entry(obj).State = EntityState.Modified;
+                dataContext.SaveChanges();
+                return Ok(obj);
+            }
+        }
+
+        [HttpDelete("DeletUser")]
+        public IActionResult DeletUser(int id )
+        {
+            var deleteUser = dataContext.Login.Find(id);
+            if (deleteUser == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                dataContext.Login.Remove(deleteUser);
+                dataContext.SaveChanges();
+                return Ok();
+            }
+        }         
+
+
+
+
+   
+    }                   
+    
+}
